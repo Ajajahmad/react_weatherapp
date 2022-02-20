56b64bdba2ed3ea2f69662deb46d574688ba2d03
Ajaj <ajaj121ahmad@gmail.com>
2022-02-20 11:22:20 +0530

    First commit

---
+++ src/Components/TempSec/Tempsec.jsx
@@ -0,0 +1,64 @@
+import React, { useEffect, useState } from 'react';
+import "./Tempsec.css";
+export default function Tempsec() {
+const [city,setCity] = useState(null);
+const [search ,setSearch] = useState("Mumbai");
+
+  const apiid = process.env.REACT_APP_WEATHER_API_KEY;
+  
+
+    useEffect( ()=>{
+      const fetchApi = async ()=>{
+        const url = `http://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=${apiid}`;
+        const response = await fetch(url);
+        // console.log(response);
+        const resJson = await response.json();
+        // console.log(resJson);
+        setCity(resJson.main);
+      };
+      fetchApi();
+    },[search])
+  return (
+    <>
+      <div className="TempsecMain">
+        <div className="Tempsecinput">
+          <label htmlFor="html">Enter City Name: </label>
+          <input
+            type="search"
+            className="inputfield"
+            name="city"
+            value={search}
+            id="city"
+            onChange={(event) => {
+              setSearch(event.target.value);
+            }}
+          ></input>
+        </div>
+        {!city ? (
+          <p className='Nodata'>No Data Found</p>
+        ) : (
+          <>
+            <div className="Tempsecbody">
+              <h2 className="location">
+                <i className="fa-solid fa-street-view icon"></i>
+                {search}
+              </h2>
+              <h1 className="temp">{city.temp}°Cel</h1>
+              <h3 className="tempmin_max">
+                Min: {city.temp_min}°Cel | Max : {city.temp_max}°Cel
+              </h3>
+            </div>
+          </>
+        )}
+
+        <div className="ocean">
+          <div className="wave"></div>
+          <div className="wave"></div>
+          <div className="wave"></div>
+          <div className="wave"></div>
+          <div className="wave"></div>
+        </div>
+      </div>
+    </>
+  );
+}
